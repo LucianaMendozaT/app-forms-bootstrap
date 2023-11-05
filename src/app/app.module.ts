ed37272221cac0f1c3a74ce954fafdca78bbc634
LucianaMendozaT <lucianamend0124@gmail.com>
2023-11-04 19:35:50 -0500

    Mensaje de confirmación descriptivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormSumaComponent } from './componentes-bs/form-suma/form-suma.component';
+import { FormListaAlumnosComponent } from './componentes-bs/form-lista-alumnos/form-lista-alumnos.component';
+import { FormCalculoImcComponent } from './componentes-bs/form-calculo-imc/form-calculo-imc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSumaComponent,
+    FormListaAlumnosComponent,
+    FormCalculoImcComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-calculo-imc',
+  templateUrl: './form-calculo-imc.component.html',
+  styleUrls: ['./form-calculo-imc.component.css']
+})
+export class FormCalculoImcComponent {
+
+}
